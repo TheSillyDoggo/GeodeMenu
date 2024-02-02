637559659081247234ade36ccacbef18876b669c
Explodingbill <talgood09@gmail.com>
2024-02-02 21:30:52 +1100

    h

--- src/Hacks/InstantComplete.cpp
+++ src/Hacks/InstantComplete.cpp
@@ -1,4 +1,4 @@
-#include <Geode/Geode.hpp>
+/*#include <Geode/Geode.hpp>
 #include <Geode/modify/GJBaseGameLayer.hpp>
 #include "../Client/Client.h"
 
@@ -21,4 +21,4 @@ class $modify (GJBaseGameLayer)
             }
         }
     }
-};
+};*/
