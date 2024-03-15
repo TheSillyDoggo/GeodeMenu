cfe2326bc0068880136d00f894f290421ec27d8a
Explodingbill <talgood09@gmail.com>
2024-03-15 20:48:30 +1100

    asdf

--- src/Hacks/PulsingTitle.cpp
+++ src/Hacks/PulsingTitle.cpp
@@ -1,5 +1,6 @@
-/*#include <Geode/Geode.hpp>
+#include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/FMODLevelVisualizer.hpp>
 #include "../Client/Client.h"
 
 #define MEMBERBYOFFSET(type, class, offset) *reinterpret_cast<type*>(reinterpret_cast<uintptr_t>(class) + offset)
@@ -10,6 +11,7 @@ using namespace geode::prelude;
 class MenuPulse : public CCNode
 {
     public:
+        CCNode* node = nullptr;
         FMOD::Channel* channel = nullptr;
 
         bool init()
@@ -25,12 +27,26 @@ class MenuPulse : public CCNode
 
         void update(float dt)
         {
-            //auto v = MBO(float, FMODAudioEngine::sharedEngine(), 0x178);
+            #ifdef GEODE_IS_ANDROID
+            node->setScale(FMODAudioEngine::sharedEngine()->getMeteringValue());
+            #endif
 
-            //log::info("volume: {}", v);
+            #ifdef GEODE_IS_WINDOWS
+            //node->setScale(2);
+            #endif
         }
 
         CREATE_FUNC(MenuPulse);
+};
+
+class $modify (FMODLevelVisualizer)
+{
+    TodoReturn updateVisualizer(float p0, float p1, float p2)
+    {
+        log::info("p0: {}, p1: {}, p2: {}", p0, p1, p2);
+
+        FMODLevelVisualizer::updateVisualizer(p0, p1, p2);
+    }
 };
 
 class $modify (MenuLayer)
@@ -40,8 +56,13 @@ class $modify (MenuLayer)
         if (!MenuLayer::init())
             return false;
 
-        this->addChild(MenuPulse::create());
+        this->addChild(FMODLevelVisualizer::create());
+
+        auto mp = MenuPulse::create();
+        mp->node = getChildOfType<CCSprite>(this, 0);
+
+        this->addChild(mp);
 
         return true;
     }
-};*/
+};
