c2dd627eda6c7897ff6690bdb5ee4549dd92d173
Explodingbill <talgood09@gmail.com>
2024-03-16 16:52:04 +1100

    ff

--- src/Hacks/SliderLimit.cpp
+++ src/Hacks/SliderLimit.cpp
@@ -8,10 +8,7 @@ class $modify (SliderTouchLogic)
     virtual void ccTouchMoved(cocos2d::CCTouch* p0, cocos2d::CCEvent* p1)
     {
         if (!Client::GetModuleEnabled("slider-limit"))
-        {
-            SliderTouchLogic::ccTouchMoved(p0, p1);
-            return;
-        }
+            return SliderTouchLogic::ccTouchMoved(p0, p1);
         
         float w = 100;
         float x = (this->convertTouchToNodeSpace(p0).x + 100) / 2;
@@ -19,9 +16,13 @@ class $modify (SliderTouchLogic)
         this->m_slider->setValue(x / w);
 
         this->m_slider->updateBar();
-            
-        #ifdef GEODE_IS_ANDROID
-        this->ccTouchEnded(p0, p1);
-        #endif
+
+        CCObject *pTarget = m_thumb->m_pListener;
+        SEL_MenuHandler pSelector = m_thumb->m_pfnSelector;
+       
+        if (pTarget && pSelector) 
+        {
+            (pTarget->*pSelector)(this);
+        }
     }
 };*/
