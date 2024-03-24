a037bc4245fce1b6f8b9079e1d06eb3312e43803
Explodingbill <talgood09@gmail.com>
2024-03-24 16:26:25 +1100

    f

--- src/Hacks/NoParticles.cpp
+++ src/Hacks/NoParticles.cpp
@@ -1,5 +1,3 @@
-#ifdef GEODE_IS_WINDOWS
-
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCParticleSystem.hpp>
 #include <Geode/modify/CCParticleSystemQuad.hpp>
@@ -9,53 +7,24 @@ using namespace geode::prelude;
 
 Module* part = nullptr;
 
-class $modify (CCParticleSystem)
+void myParticleUpdate(CCParticleSystem* ins, float dt)
 {
-    virtual bool initWithTotalParticles(unsigned int numberOfParticles, bool idk)
-    {
-        if (!part)
+    if (!part)
             part = Client::GetModule("no-particles");
-        
-        return CCParticleSystem::initWithTotalParticles(part->enabled ? 0 : numberOfParticles, idk);
-    }
+    
+    ins->update(dt);
 
-    virtual void update(float dt)
-    {
-        if (!part)
-            part = Client::GetModule("no-particles");
-        
-        if (!part->enabled)
-            CCParticleSystem::update(dt);
-        else
-            this->setScale(0);
-    }
+    if (part->enabled)
+        ins->setScale(0);
+}
 
-    virtual void setTotalParticles(unsigned int tp)
-    {
-        if (!part)
-            part = Client::GetModule("no-particles");
-        
-        CCParticleSystem::setTotalParticles(part->enabled ? 0 : tp);
-    }
-};
-
-class $modify (CCParticleSystemQuad)
-{
-    virtual bool initWithTotalParticles(unsigned int numberOfParticles, bool idk)
-    {
-        if (!part)
-            part = Client::GetModule("no-particles");
-        
-        return CCParticleSystemQuad::initWithTotalParticles(part->enabled ? 0 : numberOfParticles, idk);
-    }
-
-    virtual void setTotalParticles(unsigned int tp)
-    {
-        if (!part)
-            part = Client::GetModule("no-particles");
-        
-        CCParticleSystemQuad::setTotalParticles(part->enabled ? 0 : tp);
-    }
-};
-
-#endif
+$execute {
+    Mod::get()->hook(
+        reinterpret_cast<void*>(
+            geode::addresser::getVirtual(&CCParticleSystem::update)
+        ),
+        &myParticleUpdate,
+        "cocos2d::CCParticleSystem::update",
+        tulip::hook::TulipConvention::Thiscall
+    );
+}
