ddd4d642d3dc7bf005089d28001923d6a4826651
Explodingbill <talgood09@gmail.com>
2024-04-05 17:13:33 +1100

    Update AllModesPlatformer.cpp

--- src/Hacks/AllModesPlatformer.cpp
+++ src/Hacks/AllModesPlatformer.cpp
@@ -7,7 +7,30 @@ using namespace geode::prelude;
 
 Module* allMod = nullptr;
 
-#ifdef aGEODE_IS_WINDOWS
+void hack(GJBaseGameLayer* self, PlayerObject* p0, gd::vector<GameObject*>* p1, int p2)
+{
+    for (size_t i = 0; i < p2; i++)
+    {
+        auto obj = p1->at(i);
+
+        if (p0->getObjectRect().intersectsRect(obj->getObjectRect()))
+        {
+            if (obj->m_objectType == GameObjectType::WavePortal || obj->m_objectType == GameObjectType::SwingPortal)
+            {
+                CCScene::get()->addChild(TextAlertPopup::create("touching", 0.5f, 0.6f, 150, ""), 9999999);
+
+                if(self->canBeActivatedByPlayer(p0, as<EffectGameObject*>(obj)))
+                {
+                    self->playerWillSwitchMode(p0, obj);
+                    self->switchToFlyMode(p0, obj, false, as<int>(obj->m_objectType));
+                    obj->playShineEffect();
+                }
+            }
+        }
+    }
+}
+
+#ifdef GEODE_IS_WINDOWS
 
 class $modify (GJBaseGameLayer)
 {
@@ -18,25 +41,7 @@ class $modify (GJBaseGameLayer)
 
         if (allMod->enabled)
         {
-            for (size_t i = 0; i < p2; i++)
-            {
-                auto obj = p1->at(i);
-
-                if (p0->getObjectRect().intersectsRect(obj->getObjectRect()))
-                {
-                    if (obj->m_objectType == GameObjectType::WavePortal || obj->m_objectType == GameObjectType::SwingPortal)
-                    {
-                        CCScene::get()->addChild(TextAlertPopup::create("touching", 0.5f, 0.6f, 150, ""), 9999999);
-
-                        if(canBeActivatedByPlayer(p0, as<EffectGameObject*>(obj)))
-                        {
-                            playerWillSwitchMode(p0, obj);
-                            switchToFlyMode(p0, obj, false, as<int>(obj->m_objectType));
-                            obj->playShineEffect();
-                        }
-                    }
-                }
-            }            
+            hack(this, p0, p1, p2);
 
             return GJBaseGameLayer::collisionCheckObjects(p0, p1, p2, p3);
         }
@@ -49,6 +54,8 @@ class $modify (GJBaseGameLayer)
 
 #endif
 
+#ifdef GEODE_IS_ANDROID
+
 bool skip = false;
 
 void myCollisionCheck(GJBaseGameLayer* self, PlayerObject* p0, gd::vector<GameObject*>* p1, int p2, float p3)
@@ -59,15 +66,12 @@ void myCollisionCheck(GJBaseGameLayer* self, PlayerObject* p0, gd::vector<GameOb
 
         CCScene::get()->addChild(TextAlertPopup::create("test", 0.5f, 0.6f, 150, ""), 9999999);
 
-        //self->collisionCheckObjects(p0, p1, p2, p3);
+        self->collisionCheckObjects(p0, p1, p2, p3);
         return;
     }
 
     skip = false;
 }
-
-
-#ifdef GEODE_IS_ANDROID
 
 $execute
 {
