d8388ff91801889bb344c25a2459c04b479712be
Explodingbill <talgood09@gmail.com>
2024-05-05 15:29:48 +1000

    Update pp-frag.glsl

--- resources/pp-frag.glsl
+++ resources/pp-frag.glsl
@@ -1,6 +1,6 @@
 #version 300 es
 
-varying vec2 TexCoords;
+in vec2 TexCoords;
 
 uniform sampler2D screen;
 uniform vec2 screenSize;
@@ -8,11 +8,13 @@ uniform bool fast;
 uniform bool first;
 uniform float radius;
 
+out vec4 FragColor;
+
 void main() {
     float scaledRadius = radius * screenSize.y * 0.5;
     vec2 texOffset = 1.0 / screenSize; // gets size of single texel
 
-    vec3 result = texture2D(screen, TexCoords.st).rgb;
+    vec3 result = texture(screen, TexCoords).rgb;
     if (fast) {
         scaledRadius *= radius * 10.0 / ((radius * 10.0 + 1.0) * (radius * 10.0 + 1.0) - 1.0);
         float weight = 1.0;
@@ -21,16 +23,16 @@ void main() {
             for (int i = 1; float(i) < scaledRadius; i++) {
                 weight -= 1.0 / scaledRadius;
                 weightSum += weight * 2.0;
-                result += texture2D(screen, TexCoords.st + vec2(texOffset.x * i, 0.0)).rgb * weight;
-                result += texture2D(screen, TexCoords.st - vec2(texOffset.x * i, 0.0)).rgb * weight;
+                result += texture(screen, TexCoords + vec2(texOffset.x * float(i), 0.0)).rgb * weight;
+                result += texture(screen, TexCoords - vec2(texOffset.x * float(i), 0.0)).rgb * weight;
             }
         }
         else {
             for (int i = 1; float(i) < scaledRadius; i++) {
                 weight -= 1.0 / scaledRadius;
                 weightSum += weight * 2.0;
-                result += texture2D(screen, TexCoords.st + vec2(0.0, texOffset.y * i)).rgb * weight;
-                result += texture2D(screen, TexCoords.st - vec2(0.0, texOffset.y * i)).rgb * weight;
+                result += texture(screen, TexCoords + vec2(0.0, texOffset.y * float(i))).rgb * weight;
+                result += texture(screen, TexCoords - vec2(0.0, texOffset.y * float(i))).rgb * weight;
             }
         }
         result /= weightSum;
@@ -41,22 +43,22 @@ void main() {
         float weightSum = firstWeight;
         if (first) {
             for (int i = 1; float(i) <= ceil(scaledRadius); i++) {
-                float weight = firstWeight * exp(-i * i / (2.0 * scaledRadius));
-                weightSum += weight * 2;
-                result += texture2D(screen, TexCoords.st + vec2(texOffset.x * i, 0.0)).rgb * weight;
-                result += texture2D(screen, TexCoords.st - vec2(texOffset.x * i, 0.0)).rgb * weight;
+                float weight = firstWeight * exp(-float(i) * float(i) / (2.0 * scaledRadius));
+                weightSum += weight * 2.0;
+                result += texture(screen, TexCoords + vec2(texOffset.x * float(i), 0.0)).rgb * weight;
+                result += texture(screen, TexCoords - vec2(texOffset.x * float(i), 0.0)).rgb * weight;
             }
         }
         else {
             for (int i = 1; float(i) <= ceil(scaledRadius); i++) {
-                float weight = firstWeight * exp(-i * i / (2.0 * scaledRadius));
-                weightSum += weight * 2;
-                result += texture2D(screen, TexCoords.st + vec2(0.0, texOffset.y * i)).rgb * weight;
-                result += texture2D(screen, TexCoords.st - vec2(0.0, texOffset.y * i)).rgb * weight;
+                float weight = firstWeight * exp(-float(i) * float(i) / (2.0 * scaledRadius));
+                weightSum += weight * 2.0;
+                result += texture(screen, TexCoords + vec2(0.0, texOffset.y * float(i))).rgb * weight;
+                result += texture(screen, TexCoords - vec2(0.0, texOffset.y * float(i))).rgb * weight;
             }
         }
         result /= weightSum;
     }
 
-    gl_FragColor = vec4(result, 1.0);
+    FragColor = vec4(result, 1.0);
 }
