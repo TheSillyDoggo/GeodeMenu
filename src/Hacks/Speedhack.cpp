288ba50692b9bb526575b51b14131e2014f1cc38
Explodingbill <talgood09@gmail.com>
2024-05-17 17:36:10 +1000

    isn't ios just so fun i love how it over complicates everything

--- src/Hacks/Speedhack.cpp
+++ src/Hacks/Speedhack.cpp
@@ -1,40 +1,20 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCScheduler.hpp>
-#include "../Client/Client.h"
+#include "Speedhack.hpp"
 
 using namespace geode::prelude;
 
 FMOD::ChannelGroup* masterGroup;
-FMOD::DSP *pitchDSP = nullptr;
-InputModule* pitchAmount;
-float lastPitch = 0;
 
 float speedhackLogic(float dt)
 {
     #ifndef GEODE_IS_IOS
     if (!masterGroup)
-    {
         FMODAudioEngine::sharedEngine()->m_system->getMasterChannelGroup(&masterGroup);
-    }
     #endif
 
-    if (!pitchAmount)
-        pitchAmount = as<InputModule*>(Client::GetModule("pitch-shifter")->options[0]);
-
-    if (!masterGroup || !pitchAmount)
+    if (!masterGroup)
         return dt;
-
-    if (lastPitch != (Client::GetModuleEnabled("pitch-shifter") ? pitchAmount->getFloatValue() : 1))
-    {
-        if (pitchDSP)
-            masterGroup->removeDSP(pitchDSP);
-
-        FMODAudioEngine::sharedEngine()->m_system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &pitchDSP);
-        masterGroup->addDSP(0, pitchDSP);
-        lastPitch = Client::GetModuleEnabled("pitch-shifter") ? pitchAmount->getFloatValue() : 1;
-
-        pitchDSP->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, Client::GetModuleEnabled("pitch-shifter") ? pitchAmount->getFloatValue() : 1);
-    }
 
     ColourUtility::totalSessionTime += dt;
 
@@ -49,7 +29,9 @@ float speedhackLogic(float dt)
             if (SpeedhackGameplay::instance->enabled)
                 if (!(PlayLayer::get() || GameManager::sharedState()->getEditorLayer())) { v = 1.0f; }
 
-            #ifndef GEODE_IS_IOS //todo: this is dumb
+            #ifdef GEODE_IS_IOS
+            reinterpret_cast<FMOD_RESULT(__thiscall*)(FMOD::ChannelControl*, float)>(geode::base::get() + 0x4ffec4)(masterGroup, SpeedhackMus::instance->enabled ? v : 1);
+            #else
             masterGroup->setPitch(SpeedhackMus::instance->enabled ? v : 1);
             #endif
             ColourUtility::update(dt * v);
@@ -62,8 +44,6 @@ float speedhackLogic(float dt)
     return dt;
 }
 
-#ifdef GEODE_IS_MACOS
-
 class $modify (CCScheduler)
 {
     virtual void update(float dt)
@@ -74,14 +54,9 @@ class $modify (CCScheduler)
     }
 };
 
-#else
-
 #ifdef GEODE_IS_IOS
 
-//
-
-FMOD_RESULT MenuLayer_onNewgrounds(FMOD::System* self, const char *name, FMOD::ChannelGroup **channelgroup) {
-    //auto res = self->createChannelGroup(name, channelgroup);
+FMOD_RESULT FMOD_System_createChannelGroup(FMOD::System* self, const char *name, FMOD::ChannelGroup **channelgroup) {
     auto res = reinterpret_cast<FMOD_RESULT(__thiscall*)(FMOD::System*, const char*, FMOD::ChannelGroup**)>(geode::base::get() + 0x4c8964)(self, name, channelgroup);
 
     if (!masterGroup)
@@ -95,29 +70,9 @@ FMOD_RESULT MenuLayer_onNewgrounds(FMOD::System* self, const char *name, FMOD::C
 $execute {
     Mod::get()->hook(
         reinterpret_cast<void*>(geode::base::get() + 0x4c8964), // address
-        &MenuLayer_onNewgrounds, // detour
+        &FMOD_System_createChannelGroup, // detour
         "FMOD::System::createChannelGroup", // display name, shows up on the console
         tulip::hook::TulipConvention::Thiscall // calling convention
-    );
-}
-
-#endif
-
-void myUpdate(CCScheduler* ins, float dt)
-{
-    dt = speedhackLogic(dt);
-    
-    ins->update(dt);
-}
-
-$execute {
-    Mod::get()->hook(
-        reinterpret_cast<void*>(
-            geode::addresser::getVirtual(&CCScheduler::update)
-        ),
-        &myUpdate,
-        "cocos2d::CCScheduler::update",
-        tulip::hook::TulipConvention::Thiscall
     );
 }
 
---
+++ src/Hacks/Speedhack.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include "../Client/Client.h"
+
+using namespace geode::prelude;
+
+extern FMOD::ChannelGroup* masterGroup;
