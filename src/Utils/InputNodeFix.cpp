918b05ee7db283d15a5c6438266d126112fe4803
Explodingbill <talgood09@gmail.com>
2024-05-19 11:55:43 +1000

    Update InputNodeFix.cpp

--- src/Utils/InputNodeFix.cpp
+++ src/Utils/InputNodeFix.cpp
@@ -7,7 +7,7 @@ using namespace geode::prelude;
 class $modify (CCTextInputNode)
 {
     bool ccTouchBegan(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) {
-        if (!this->getUserObject("fix-text-input") || !(this->getID().starts_with("IGNOREBYPASSES"_spr))) {
+        if (!(this->getID().starts_with("IGNOREBYPASSES"_spr))) {
             return CCTextInputNode::ccTouchBegan(touch, event);
         }
 
@@ -18,20 +18,21 @@ class $modify (CCTextInputNode)
 
         auto const touchPos = touch->getLocation();
         auto const size = this->getContentSize();
-        auto const pos = this->convertToNodeSpace(touchPos) + m_textField->getAnchorPoint() * size;
 
-        if (pos.x < 0 || pos.x > size.width || pos.y < 0 || pos.y > size.height) {
-            this->onClickTrackNode(false);
-            return false;
+        if (this->boundingBox().containsPoint(touchPos))
+        {
+            this->onClickTrackNode(true);
+            this->updateCursorPosition(touchPos, {{0, 0}, size});
+
+            return true;
         }
+
         if (m_delegate && !m_delegate->allowTextInput(this)) {
             this->onClickTrackNode(false);
             return false;
         }
 
-        this->onClickTrackNode(true);
-        this->updateCursorPosition(touchPos, {{0, 0}, size});
-
-        return true;
+        this->onClickTrackNode(false);
+        return false;
     }
 };
