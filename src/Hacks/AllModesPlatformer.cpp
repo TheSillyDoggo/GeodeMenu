230b4fa5355deb8e38dd0102504daa994a6a66c2
TheSillyDoggo <95105680+TheSillyDoggo@users.noreply.github.com>
2024-08-28 08:14:23 +1000

    Update AllModesPlatformer.cpp

--- src/Hacks/AllModesPlatformer.cpp
+++ src/Hacks/AllModesPlatformer.cpp
@@ -11,6 +11,11 @@ class $modify (GJBaseGameLayer)
 {
     void collisionCheckObjects(PlayerObject* p0, gd::vector<GameObject*>* p1, int p2, float p3)
     {
+GJBaseGameLayer::collisionCheckObjects(p0, p1, p2, p3);
+
+if (!m_isPlatformer)
+ return;
+
         if (p0 && p1)
         {
             for (size_t i = 0; i < p1->size(); i++)
@@ -42,8 +47,6 @@ if (obj->m_objectType == GameObjectType::WavePortal || obj->m_objectType == Game
                 }
             }
         }
-
-        GJBaseGameLayer::collisionCheckObjects(p0, p1, p2, p3);
     }
 
     QOLMOD_MOD_HOOK("all-plat", "GJBaseGameLayer::collisionCheckObjects")
