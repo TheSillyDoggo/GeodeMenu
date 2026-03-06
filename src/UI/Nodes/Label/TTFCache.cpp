521a59432375c1c960ce71951d1c79ad8ee5ed82
thesillydoggo <talgood09@gmail.com>
2026-03-06 21:25:44 +1100

    almost forgot about ios :c

--- src/UI/Nodes/Label/TTFCache.cpp
+++ src/UI/Nodes/Label/TTFCache.cpp
@@ -33,12 +33,25 @@ void TTFCache::addTexture(std::string text)
 
     std::string fontName = "Arial.ttf";
 
+    #if GEODE_COMP_GD_VERSION == 22074
+    #ifdef GEODE_IS_IOS
+    tex = CCLabelTTF::create(text2.c_str(), fontName.c_str(), 16)->getTexture();
+    #else
     tex->initWithString(text2.c_str(),
         fontName.c_str(),
         16 * CC_CONTENT_SCALE_FACTOR(),
         CC_SIZE_POINTS_TO_PIXELS(CCSizeZero),
         CCTextAlignment::kCCTextAlignmentLeft,
         kCCVerticalTextAlignmentCenter);
+    #endif
+    #else
+    tex->initWithString(text2.c_str(),
+        fontName.c_str(),
+        16 * CC_CONTENT_SCALE_FACTOR(),
+        CC_SIZE_POINTS_TO_PIXELS(CCSizeZero),
+        CCTextAlignment::kCCTextAlignmentLeft,
+        kCCVerticalTextAlignmentCenter);
+    #endif
 
     stringsCached.emplace(text, tex);
 }
